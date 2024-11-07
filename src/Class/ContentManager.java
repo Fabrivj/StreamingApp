ddf977757d7ec3e3d9acf1ba77782228eb55e33e
AndreyBM12213 <abonillam@ucenfotec.ac.cr>
2024-11-07 12:05:16 -0600

    Update ContentManager.java

--- src/Class/ContentManager.java
+++ src/Class/ContentManager.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class ContentManager {
     private List<ContentItems> contentList;
 
+    
     // Constructor
     public ContentManager() {
         contentList = new ArrayList<>();
